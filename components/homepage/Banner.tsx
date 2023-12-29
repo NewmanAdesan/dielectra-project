100064b4e708197ccac6788860f1263aeefbae50
NewmanAdesan <newmanadesan@gmail.com>
2023-12-29 18:14:53 +0100

    Refactored the State Management of the Banner Carousel. Changed from useState to useReducer

--- components/homepage/Banner.tsx
+++ components/homepage/Banner.tsx
@@ -1,40 +1,102 @@
 'use client'
 
 import Image from 'next/image'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useReducer, useState } from 'react'
 import styles from './Banner.module.css'
 
+const NUMBER_OF_BANNER_CAROUSEL_IMAGE = 5;
+
+type BannerInitialState = {
+  activeImage: number,
+  howManyImagesHasLoaded: number,
+  intervalID: number | null,
+}
+
+type DispatchAction = {
+  type: 'images-loaded' | 'loaded-an-image' | 'clear-interval' | 'setup-interval' | 'set-active-image' | 'slide-to-next-active-image',
+  payload?: number
+}
+
+const initialState : BannerInitialState = {
+
+  // this state tracks which banner carousel image is active 
+  // initially no banner carousel image is active 
+  // BECAUSE According to the design, Banner Carousel Images start downloading only after the Banner Component has mounted.
+  activeImage: -1,
+
+  // this state controls when the banner carousel images starts downloading 
+  // when it is -1, no banner carousel image will download BECAUSE we want the images to start downloading when the Banner Component has finished mouting
+  // when it is 0, it signifies no image has downloaded thus the first image will begin downloading 
+  howManyImagesHasLoaded: -1, 
+
+  // this state keeps track of the interval that controls the carousel animation
+  intervalID: null,
+} 
+
+function reducer(state: BannerInitialState, action: DispatchAction): BannerInitialState {
+  switch(action.type) {
+    case 'images-loaded':
+      return {
+        ...state,
+        howManyImagesHasLoaded: Number(action.payload)
+      }
+
+    case 'loaded-an-image':
+      return {
+        ...state,
+        howManyImagesHasLoaded: state.howManyImagesHasLoaded + 1,
+      }
+    
+    case 'clear-interval':
+      console.log('previous interval was cleared');
+      return {
+        ...state,
+        intervalID: state.intervalID ? Number(clearInterval(state.intervalID)) : state.intervalID
+      }
+
+    case 'setup-interval':
+      return {
+        ...state,
+        intervalID: Number(action.payload)
+      }
+
+    case 'set-active-image': 
+      return {
+        ...state,
+        activeImage: Number(action.payload)
+      }
+
+    case 'slide-to-next-active-image':
+      return {
+        ...state,
+        activeImage: (state.activeImage + 1) % NUMBER_OF_BANNER_CAROUSEL_IMAGE
+      }
+    
+    default:
+      throw new Error('Action Type is not supported');
+  }
+}
 
 const Banner = () => {
-  const [activeImage, setActiveImage] = useState(-1);
-  const [howManyImagesHasLoaded, setHowManyImagesHasLoaded] = useState(-1);
-  const [intervalID, setIntervalID] = useState<number | null>(null);
+  const [{activeImage, howManyImagesHasLoaded}, dispatch] = useReducer(reducer, initialState)
 
   useEffect(() => {
-    console.log('useEffect has started');
-    setHowManyImagesHasLoaded(0);
+    dispatch({type: 'images-loaded', payload: 0});
     return () => {
-      console.log('useEffect removed')
-      setHowManyImagesHasLoaded(-1);
+      dispatch({type: 'images-loaded', payload: -1});;
     }
   }, [])
 
   function startInterval() {
-
     console.log('the startInterval is called')
-
-     if (intervalID) {
-      console.log('previous interval was cleared')
-      clearInterval(intervalID)
-    };
-     setIntervalID(
-        Number(
-          setInterval(()=>{
-            console.log('new interval has started');
-            setActiveImage(prev => (prev + 1) % 5);
-          }, 5000)
-        )
-     )
+    dispatch({type: 'clear-interval'})
+    const id = Number(
+      setInterval(()=>{
+        console.log('new interval has started');
+        dispatch({type: 'slide-to-next-active-image'})
+      }, 5000)
+    )
+    dispatch({type: 'setup-interval', payload: id})
   }
   
   return (
@@ -53,8 +115,8 @@ const Banner = () => {
                    }}
                    onLoad={() => {
                       console.log('first banner image has loaded');
-                      setHowManyImagesHasLoaded( prev => prev + 1);
-                      setActiveImage(0);
+                      dispatch({type: 'loaded-an-image'})
+                      dispatch({type: 'set-active-image', payload: 0})
                    }}
             />}
             {howManyImagesHasLoaded >= 1 && <Image src='/homepage/banner-image-2.png'
@@ -69,7 +131,7 @@ const Banner = () => {
                    }}
                    onLoad={() => {
                       console.log('second banner image has loaded');
-                      setHowManyImagesHasLoaded( prev => prev + 1);
+                      dispatch({type: 'loaded-an-image'})
                    }}
             />}
             {howManyImagesHasLoaded >= 2 && <Image src='/homepage/banner-image-3.png'
@@ -84,7 +146,7 @@ const Banner = () => {
                    }}
                    onLoad={() => {
                       console.log('thrid banner image has loaded');
-                      setHowManyImagesHasLoaded( prev => prev + 1);
+                      dispatch({type: 'loaded-an-image'})
                    }}
             />}
             {howManyImagesHasLoaded >= 3 && <Image src='/homepage/banner-image-4.png'
@@ -99,7 +161,7 @@ const Banner = () => {
                    }}
                    onLoad={() => {
                       console.log('banner image 4 has loaded');
-                      setHowManyImagesHasLoaded( prev => prev + 1);
+                      dispatch({type: 'loaded-an-image'})
                    }}
             />}
             {howManyImagesHasLoaded >= 4 && <Image src='/homepage/banner-image-5.png'
@@ -114,7 +176,7 @@ const Banner = () => {
                    }}
                    onLoad={() => {
                       console.log('banner image 5 has loaded');
-                      setHowManyImagesHasLoaded( prev => prev + 1);
+                      dispatch({type: 'loaded-an-image'})
                       startInterval();
                    }}
             />}
@@ -130,23 +192,23 @@ const Banner = () => {
         <div className='absolute bottom-10 left-1/2 -translate-x-1/2 '>
           <div className='flex gap-1 justify-center w-20 rounded-lg bg-black p-1'>
               <button className={`${activeImage === 0 ? 'w-4 bg-neutral-200' : 'w-2 bg-neutral-500'} h-2  rounded-full hover:bg-neutral-200 transition-all duration-300 ease-in-out`} onClick={() => {
-                  setActiveImage(0);
+                  dispatch({type: 'set-active-image', payload: 0})
                   startInterval();
                 }}></button>
               <button className={`${activeImage === 1 ? 'w-4 bg-neutral-200' : 'w-2 bg-neutral-500'} h-2  rounded-full hover:bg-neutral-200 transition-all duration-300 ease-in-out`} onClick={() => {
-                  setActiveImage(1);
+                  dispatch({type: 'set-active-image', payload: 1});
                   startInterval();
                 }}></button>
               <button className={`${activeImage === 2 ? 'w-4 bg-neutral-200' : 'w-2 bg-neutral-500'} h-2  rounded-full hover:bg-neutral-200 transition-all duration-300 ease-in-out`} onClick={() => {
-                  setActiveImage(2);
+                  dispatch({type: 'set-active-image', payload: 2});
                   startInterval();
                 }}></button>
               <button className={`${activeImage === 3 ? 'w-4 bg-neutral-200' : 'w-2 bg-neutral-500'} h-2  rounded-full hover:bg-neutral-200 transition-all duration-300 ease-in-out`} onClick={() => {
-                  setActiveImage(3);
+                  dispatch({type: 'set-active-image', payload: 3});
                   startInterval();
                 }}></button>
               <button className={`${activeImage === 4 ? 'w-4 bg-neutral-200' : 'w-2 bg-neutral-500'} h-2  rounded-full hover:bg-neutral-200 transition-all duration-300 ease-in-out`} onClick={() => {
-                  setActiveImage(4);
+                  dispatch({type: 'set-active-image', payload: 4});
                   startInterval();
                 }}></button>
           </div>
